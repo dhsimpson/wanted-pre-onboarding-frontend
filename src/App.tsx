7abd4b26a662d596bc67acb48323ccbed1d31f31
dhsimpson <qeadzc9503@naver.com>
2023-02-04 02:13:21 +0900

    feat | router signin, signup, todo 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Signup from 'pages/Signup'
+import Signin from 'pages/Signin'
+import Todo from 'pages/Todo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* TODO : / 경로로 온 경우엔 어디로 이동시킬까?? /todo 를 기본 경로로 하는 방법은 없을까? */}
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/todo" element={<Todo />}></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
