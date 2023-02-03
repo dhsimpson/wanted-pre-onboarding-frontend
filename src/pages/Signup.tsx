2c0ebbb39536dee0e53c67b167719eb4dcf5943a
dhsimpson <qeadzc9503@naver.com>
2023-02-04 06:15:44 +0900

    feat | 이메일/패스워드 오입력으로 인한 에러 처리

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -6,11 +6,26 @@ export default function Signup() {
   const { handleSubmit, handleChange, setEmail, setPassword, isValid } =
     useAuthForm(async (target: IAuthFormData) => {
       // axios
-      const res = await axiosClient.post('auth/signup', {
-        email: target.email.value,
-        password: target.password.value
-      })
-      console.log(res)
+      try {
+        const res = await axiosClient.post('auth/signup', {
+          email: target.email.value,
+          password: target.password.value
+        })
+
+        if (res.status === 201) {
+          alert('회원가입 성공!')
+        }
+      } catch (error: any) {
+        if (error.response.status === 400) {
+          //error.response.message
+          alert('이미 있는 이메일 입니다.')
+          return
+        }
+        if (error.response.status === 401) {
+          alert('이메일 혹은 패스워드를 잘못 입력했습니다.')
+          return
+        }
+      }
     })
   return (
     <form onSubmit={handleSubmit}>
