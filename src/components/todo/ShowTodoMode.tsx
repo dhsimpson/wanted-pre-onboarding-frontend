b50ce062897f0fce684909ea0c05bc77b0a44ea2
dhsimpson <qeadzc9503@naver.com>
2023-02-04 16:54:20 +0900

    feat | todo 삭제기능 구현

--- src/components/todo/ShowTodoMode.tsx
+++ src/components/todo/ShowTodoMode.tsx
@@ -1,3 +1,4 @@
+import axiosClient from 'customClients/axiosClient'
 import { ITodoData } from 'interfaces/ITodoData'
 
 export default function ShowTodoMode({
@@ -7,6 +8,25 @@ export default function ShowTodoMode({
   todoItem: ITodoData
   setIsUpdateMode: React.Dispatch<React.SetStateAction<boolean>>
 }) {
+  async function handleDelete() {
+    const willDelete = confirm('정말 삭제하시겠습니까?')
+    if (willDelete) {
+      try {
+        const response = await axiosClient.delete(`todos/${todoItem.id}`)
+        if (response.status === 204) {
+          alert('삭제 완료되었습니다!')
+        }
+      } catch (error: any) {
+        const errorStatus = error.response?.status
+        // if (errorStatus === 401) {
+        //   alert('다시 로그인 해주세요!')
+        // }
+        // return errorStatus
+        alert(errorStatus + '!!')
+        console.error(error)
+      }
+    }
+  }
   return (
     <>
       <label>
@@ -16,7 +36,9 @@ export default function ShowTodoMode({
       <button data-testid="modify-button" onClick={() => setIsUpdateMode(true)}>
         수정
       </button>
-      <button data-testid="delete-button">삭제</button>
+      <button data-testid="delete-button" onClick={handleDelete}>
+        삭제
+      </button>
     </>
   )
 }
