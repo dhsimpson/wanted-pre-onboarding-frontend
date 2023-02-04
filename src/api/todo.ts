c26ccba3efe3ed15382310e35f1c8a7fe3f07be4
dhsimpson <qeadzc9503@naver.com>
2023-02-05 00:41:21 +0900

    refactor | addTodo 로직의 api call 로직을 나눔 (api 호출 로직과 비호출 로직의 분리)

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -11,6 +11,20 @@ export const getTodoList = async () => {
       alert('다시 로그인 해주세요!')
       deleteToken()
     }
+    console.error(error)
     return errorStatus
+  }
+}
+
+export const addTodoList = async (todo: string) => {
+  try {
+    const response = await axiosClient.post('todos', {
+      todo
+    })
+    return response
+  } catch (error: any) {
+    const errorStatus = error.response?.status
+    alert(errorStatus + '!!')
+    console.error(error)
   }
 }
--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -1,4 +1,4 @@
-import axiosClient from 'customClients/axiosClient'
+import { addTodoList } from 'api/todo'
 import { ITodoFormData } from 'interfaces/ITodo'
 import { FormEvent } from 'react'
 import { canSubmit } from 'utils/validateTodo'
@@ -10,27 +10,17 @@ export default function useAddTodoList() {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const target = event.target as ITodoFormData
+    if (!canSubmit(target.todo.value)) {
+      alert('todo 가 입력되지 않았습니다!')
+      return
+    }
 
-    try {
-      if (!canSubmit(target.todo.value)) {
-        alert('todo 가 입력되지 않았습니다!')
-        return
-      }
-
-      const response = await axiosClient.post('todos', {
-        todo: target.todo.value
-      })
-
-      if (response.status === 201) {
+    addTodoList(target.todo.value).then((response) => {
+      if (response?.status === 201) {
         alert('todo 가 추가되었습니다!')
-        console.log(response)
         setContextTodoList?.([...(contextTodoList ?? []), response.data])
       }
-    } catch (error: any) {
-      const errorStatus = error.response?.status
-      alert(errorStatus + '!!')
-      console.error(error)
-    }
+    })
   }
   return handleSubmit
 }
