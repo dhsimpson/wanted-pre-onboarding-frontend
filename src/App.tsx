14298e068e5e6e31169dc4ca9a6982193da89b12
윤동희 <qeadzc9503@naver.com>
2023-02-06 03:27:10 +0900

    test end

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Todo from 'pages/Todo'
 
 function App() {
   return (
-    <div className="App2">
+    <div className="App">
       <Routes>
         {/* TODO : / 경로로 온 경우엔 어디로 이동시킬까?? /todo 를 기본 경로로 하는 방법은 없을까? */}
         <Route path="/signup" element={<Signup />}></Route>
