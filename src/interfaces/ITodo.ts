6d53327d0e693ec9b241edfd3b59fe142950a879
윤동희 <qeadzc9503@naver.com>
2023-02-07 02:33:43 +0900

    refactor | axios 요청, 응답 타입 지정

--- src/interfaces/ITodo.ts
+++ src/interfaces/ITodo.ts
@@ -8,3 +8,20 @@ export interface ITodoData {
 export interface ITodoFormData extends EventTarget {
   todo: { value: string }
 }
+
+// axios 에서 사용하는 타입
+export interface ITodoAddRequest {
+  todo: string
+}
+
+export interface ITodoListResponse extends IDefaultResponse {
+  data: ITodoData[]
+}
+
+export interface ITodoDataResponse extends IDefaultResponse {
+  data: ITodoData
+}
+
+export interface IDefaultResponse {
+  status: number
+}
